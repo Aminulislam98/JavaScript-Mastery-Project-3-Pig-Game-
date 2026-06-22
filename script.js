a7a1b623ce1a6786391c7d1d5b59a775e8c4f973
Aminul islam <aminurrohman98@gmail.com>
2026-06-22 23:35:12 +0100

    stored all button element in variable , and made dice element dynamic

--- script.js
+++ script.js
@@ -2,7 +2,22 @@
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
+// Starting condition
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Rolling dice functionality;
+btnRoll.addEventListener('click', function () {
+  // Generate the random number between 1 and 6;
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // Display roll dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+});
