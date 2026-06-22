74ad9d2a8473a1413a186511aa1dad4c9b57b771
Aminul islam <aminurrohman98@gmail.com>
2026-06-22 23:50:16 +0100

    using if else by own displayed the dice number and stored in global variable

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -11,6 +13,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+// Storing the dice score in variable ;
+let currentScore = 0;
+
 // Rolling dice functionality;
 btnRoll.addEventListener('click', function () {
   // Generate the random number between 1 and 6;
@@ -20,4 +25,11 @@ btnRoll.addEventListener('click', function () {
   // Display roll dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    currentScore = 0;
+    current0El.textContent = currentScore;
+  }
 });
