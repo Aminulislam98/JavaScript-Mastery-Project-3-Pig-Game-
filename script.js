61a75367ce781dd123215e94868e8556d865e524
Aminul islam <aminurrohman98@gmail.com>
2026-06-23 13:22:12 +0100

    encapsulate player switching and score resetting logic into functions

--- script.js
+++ script.js
@@ -24,26 +24,49 @@ let activePlayer = 0;
 // Storing the dice score in variable ;
 let currentScore = 0;
 
+// Switch the player
+const switchPlayer = function () {
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// turning the current player current score 0
+const currentScoreZero = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+};
+
+// new game , reset al score;
+
+const playNewGame = function () {
+  currentScoreZero();
+  globalScore[0] = 0;
+  globalScore[1] = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+};
+
 // Rolling dice functionality;
 btnRoll.addEventListener('click', function () {
   // Generate the random number between 1 and 6;
   const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
 
   // Display roll dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+
+  // checking the player dice
   if (dice !== 1) {
     currentScore += dice;
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
-  } else {
-    // Switch Player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+  }
+  // Switch Player
+  else {
+    currentScoreZero();
+    switchPlayer();
   }
 });
 
@@ -57,11 +80,11 @@ btnHold.addEventListener('click', function () {
     globalScore[activePlayer];
 
   // turning the current player current score 0
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  currentScore = 0;
+  currentScoreZero();
 
   // Switching the player
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
+  switchPlayer();
 });
+
+// Play new game;
+btnNew.addEventListener('click', playNewGame);
